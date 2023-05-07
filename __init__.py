6ebfc8453b80b441c54ec9288120de24aedf9cf3
崔家诚 <Pears0n@foxmail.com>
2023-05-07 21:35:38 +0800

    Update __init__.py
    增加自动导入模块

--- __init__.py
+++ __init__.py
@@ -1,14 +1,21 @@
 import information
 from plugins import Robot
-from plugins import greeting
+import os
+import importlib
 
 if __name__ == '__main__':
     q = information.run()
     Robot = Robot()
-    print(Robot.url)
-    Robot.add_plugin(plugin=greeting)
-    print(Robot.plugins)
-
+    modules = [f[:-3] for f in os.listdir('plugins') if f.endswith('.py')]
+    modules.remove("__init__")
+    print(modules)
+    for module_name in modules:
+        module = importlib.import_module(f'plugins.{module_name}')
+        Robot.add_plugin(plugin=module)
     while True:
-        message = q.get()
-        Robot.handle_message(message=message)
+        try:
+            message = q.get()
+            print(message)
+            Robot.handle_message(message=message)
+        except:
+            pass
