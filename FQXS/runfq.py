f0fbd3e221023ea2a56a5df06d7ae4a68cc47856
崔家诚 <Pears0n@foxmail.com>
2023-06-24 19:23:29 +0800

    Add files via upload

---
+++ FQXS/runfq.py
@@ -0,0 +1,14 @@
+from fqxs import *
+
+"""番茄小说,安卓需要替换so
+IOS抓邀请好友页面
+低保大约1r/d
+变量名称fqxs  -> url#cookie#argus#ladon@ <-多账号
+            fqxsua -> 随便编一个ua
+            
+使用前填入用户名和key
+"""
+
+name = ""
+key = ""
+check(name, key)
