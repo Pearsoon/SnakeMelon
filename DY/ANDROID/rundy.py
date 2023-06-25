46615c8053ea13b9d65c6d44315508f82fb83dfc
崔家诚 <Pears0n@foxmail.com>
2023-06-25 21:04:50 +0800

    Add files via upload

---
+++ DY/ANDROID/rundy.py
@@ -0,0 +1,32 @@
+"""抖音极速版
+by Pearson
+https://github.com/Pears0nLee/SnakeMelon
+bug提交https://t.me/+T8ozejX9rnkwZDE1
+低保大约1r/d看号
+变量名  dyjsb  -> url#cookie#argus#ladon@ <-多账号
+            jsbua -> 随便编一个ua
+"""
+import platform
+import os
+
+name = ""
+key = ""
+
+
+def load_so_file():
+    system = platform.system().lower()
+    if system == 'windows':
+        file_ext = 'pyd'
+    else:
+        file_ext = 'so'
+    machine_type = platform.machine().replace('aarch64', 'arm').replace('x86_64', 'amd').replace('AMD64', 'amd')
+    python_version = f"py{platform.python_version_tuple()[0]}{platform.python_version_tuple()[1]}"
+    so_filename = f"dy_android_{system}_{machine_type}_{python_version}.{file_ext}"
+    os.system(f'cp ./{so_filename} ./dy_android.{file_ext}')
+    return so_filename
+
+
+print(load_so_file())
+from dy_andioid import *
+
+check(name, key)
