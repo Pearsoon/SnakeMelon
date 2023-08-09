f8111035f5ea3130d49f22532145a41289e50722
Pearson <Pears0n@foxmail.com>
2023-08-09 22:01:01 +0800

    Update runfq.py

--- FQXS/runfq.py
+++ FQXS/runfq.py
@@ -1,32 +1,52 @@
-"""番茄小说,安卓需要替换so
-IOS抓邀请好友页面
-低保大约1r/d
-变量名称fqxs  -> url#cookie#argus#ladon@ <-多账号
-            fqxsua -> 随便编一个ua
-            
-使用前填入用户名和key
+"""
+今日头条(注意不是极速版)
+by Pearson
+搜索 done/excitation_ad/ 抓该接口的url(...done/excitation_ad/?后的url)、cookie 、argus、ladon
+cookies格式 jrtt = "url#cookie#argus#ladon#ua" 多账号@
+小卖铺 http://www.pearsons.live/ 将收到的卡密
+    name              key              账号限制         到期时间
+{'Pearson': {'key': 'abcd123', 'chance': 3, 'data': '20230830'}
+填到name = "Pearson"   key = "abcd123"
+
+注意⚠ arm用户请用Debian版青龙
+docker run -dit \
+   -v $PWD/ql/data:/ql/data \
+   -p 5600:5700 \
+   --name qinglongdebian \
+   --hostname qinglong \
+   --restart always \
+   whyour/qinglong:debian
+
 """
 import platform
 import os
 
 name = ""
 key = ""
+githubProxy = "https://pears0n.xyz/"  # GitHub加速站
 
 
-def load_so_file():
+def load_so_file(filename):
+    files = os.listdir()
     system = platform.system().lower()
     if system == 'windows':
         file_ext = 'pyd'
     else:
         file_ext = 'so'
-    machine_type = platform.machine().replace('aarch64', 'arm').replace('x86_64', 'amd').replace('AMD64', 'amd')
-    python_version = f"py{platform.python_version_tuple()[0]}{platform.python_version_tuple()[1]}"
-    so_filename = f"fqxs_{system}_{machine_type}_{python_version}.{file_ext}"
-    os.system(f'cp ./{so_filename} ./fqxs.{file_ext}')
-    return so_filename
+    if f"{filename}.{file_ext}" not in files:
+        machine_type = platform.machine().replace('aarch64', 'arm').replace('x86_64', 'amd').replace('AMD64', 'amd')
+        python_version = f"py{platform.python_version_tuple()[0]}{platform.python_version_tuple()[1]}"
+        so_filename = f"{filename}_{system}_{machine_type}_{python_version}.{file_ext}"
+        os.system(f'cp ./{so_filename} ./{filename}.{file_ext}')
+        os.system(
+            f'curl -LjO {githubProxy}https://raw.githubusercontent.com/Pears0nLee/SnakeMelon/master/{filename.upper()}/{so_filename}')
 
+        os.system(f'cp ./{so_filename} ./{filename}.{file_ext}')
 
-print(load_so_file())
-from fqxs import *
+        return so_filename
 
-check(name, key)
+load_so_file("fqxx")
+
+from fqxx import *
+
+authorization(name, key)
